772cb174f2174c7a324e3f67cde5cad04a39c7d4
程明旺 <1002113844@qq.com>
2019-11-18 14:28:39 +0800

    创建了App.js文件

---
+++ 3-使用脚手架/react-learn/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            这是一个App
+        </div>
+    );
+}
+
+export default App;
--- 3-使用脚手架/react-learn/src/index.js
+++ 3-使用脚手架/react-learn/src/index.js
@@ -1,20 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MyFuncComp from "./MyFuncComp.js";
-import MyClassComp from "./MyClassComp.js";
+import App from "./App.js";
 
-console.log(MyClassComp);
-ReactDOM.render(
-  <div>
-    <p>
-      <MyFuncComp number="2" age={20} />
-      <MyFuncComp number={5} />
-    </p>
 
-    <p>
-      <MyClassComp number="2" age={20} ennabel />
-      <MyClassComp number="2" age={20} obj={{ name: "cmw", age: 18 }} ennabel />
-    </p>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
