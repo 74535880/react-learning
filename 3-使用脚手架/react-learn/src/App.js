c554be38b3fe5dd965f074496b0ceaec8894234d
程明旺 <1002113844@qq.com>
2019-11-27 22:15:58 +0800

    Transition -1

--- 3-使用脚手架/react-learn/src/App.js
+++ 3-使用脚手架/react-learn/src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Transition } from "react-transition-group";
+import "./App.css";
+const duration = 700;
 
-const App = () => {
-    return (
-        <div>
-            这是一个App
-        </div>
-    );
+let a = 0;
+function App() {
+  const [inProp, setInProp] = useState(true);
+  return (
+    <div>
+      <Transition
+        in={inProp}
+        timeout={duration}
+        mountOnEnter={true}
+        unmountOnExit={true}
+        appear={true}
+        addEndListener={(node, done) => {
+          console.log(node, done);
+          done();
+          node.addEventListener("transitionend", done, false);
+        }}
+      >
+        {state => {
+          a++;
+          console.log(a);
+          console.log(state);
+          return (
+            <div
+              className={state}
+              //   style={{
+              //     ...defaultStyle,
+              //     ...transitionStyles[state]
+              //   }}
+            >
+              I'm a fade Transition!，{a}
+              <h1 className={state}></h1>
+            </div>
+          );
+        }}
+      </Transition>
+      <button
+        onClick={() => {
+          setInProp(!inProp);
+        }}
+      >
+        Click to Enter
+      </button>
+    </div>
+  );
 }
-
 export default App;
