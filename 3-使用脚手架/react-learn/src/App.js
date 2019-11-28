3fa13293b336a68fb52a642d37a9bc172e33ea7f
程明旺 <1002113844@qq.com>
2019-11-28 14:49:05 +0800

    master

--- 3-使用脚手架/react-learn/src/App.js
+++ 3-使用脚手架/react-learn/src/App.js
@@ -1,46 +1,11 @@
-import React, { useState } from "react";
-import uuid from "uuid";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import "./App.css";
-const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: uuid(),
-      name: "任务1"
-    },
-    {
-      id: uuid(),
-      name: "任务2"
-    }
-  ]);
+import React from 'react';
 
+const App = () => {
   return (
     <div>
-      <TransitionGroup component="ul" className="abc">
-        {tasks.map(t => (
-          <CSSTransition key={t.id} timeout={2000}>
-            <div>
-              {t.name}
-              <button
-                onClick={() => {
-                  setTasks(tasks.filter(it => it.id !== t.id));
-                }}
-              >
-                删除
-              </button>
-            </div>
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
-      <button
-        onClick={() => {
-          setTasks([...tasks, { id: uuid(), name: `任务` }]);
-        }}
-      >
-        添加一个任务
-      </button>
+      
     </div>
   );
-};
+}
 
 export default App;
